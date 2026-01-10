bdad474678fea3dc9daf502ea8cede132ddb6404
Shubham Shinde <shubhamshinde225@gmail.com>
2026-01-10 16:59:26 +0530

    feat: new view added to show selected components those can be imported and extracted directly

--- src/component-data-provider.ts
+++ src/component-data-provider.ts
@@ -1,30 +1,40 @@
-import * as path from "path";
 import * as vscode from "vscode";
 import axios from "axios";
 
+export interface Component {
+  type: string;
+  package: string;
+  module: string;
+  code: string;
+}
+
 export class TreeNode extends vscode.TreeItem {
+  public readonly component?: Component;
+
   constructor(
     public readonly label: string,
+    public readonly componentType: string = "",
+    public readonly description: string = "",
     public readonly contextType: string,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-    public readonly children: TreeNode[] = []
+    public readonly children: TreeNode[] = [],
+    component?: Component
   ) {
     super(label, collapsibleState);
-    const iconFile =
+    this.component = component;
+    const iconId =
       {
-        rootNode: "boolean.svg",
-        folderNode: "folder.svg",
-      }[contextType] ?? "infor-ln-logo.svg";
+        rootNode:
+          componentType === "Table"
+            ? "table"
+            : componentType === "Session"
+            ? "window"
+            : "code",
+        folderNode: "folder",
+      }[contextType] ?? "file-code";
 
-    this.iconPath = path.join(
-      __filename,
-      "..",
-      "..",
-      "resources",
-      "dark",
-      iconFile
-    );
     this.contextValue = children.length > 0 ? "folderNode" : "leafNode";
+    this.iconPath = new vscode.ThemeIcon(iconId);
   }
 }
 
@@ -39,6 +49,8 @@ export class ComponentDataProvider
   > = this._onDidChangeTreeData.event;
 
   private data: TreeNode[] = [];
+  private selectedComponents: Set<TreeNode> = new Set();
+  private selectionChangeListeners: (() => void)[] = [];
 
   refresh(data: TreeNode[]) {
     this.data = data;
@@ -51,6 +63,78 @@ export class ComponentDataProvider
 
   getChildren(element?: TreeNode): Thenable<TreeNode[]> {
     return Promise.resolve(element ? element.children : this.data);
+  }
+
+  toggleSelection(node: TreeNode) {
+    if (node.component && node.contextType === "leafNode") {
+      if (this.selectedComponents.has(node)) {
+        this.selectedComponents.delete(node);
+      } else {
+        this.selectedComponents.add(node);
+      }
+      this._onDidChangeTreeData.fire();
+      this.notifySelectionChange();
+    }
+  }
+
+  isSelected(node: TreeNode): boolean {
+    return this.selectedComponents.has(node);
+  }
+
+  getSelectedComponents(): Component[] {
+    const components: Component[] = [];
+    this.selectedComponents.forEach((node) => {
+      if (node.component) {
+        components.push(node.component);
+      }
+    });
+    return components;
+  }
+
+  getSelectedNodes(): TreeNode[] {
+    return Array.from(this.selectedComponents);
+  }
+
+  removeSelectedNode(node: TreeNode) {
+    // Find and remove the node by component data (in case node references differ after refresh)
+    if (node.component) {
+      for (const selectedNode of this.selectedComponents) {
+        if (
+          selectedNode.component &&
+          selectedNode.component.type === node.component.type &&
+          selectedNode.component.package === node.component.package &&
+          selectedNode.component.module === node.component.module &&
+          selectedNode.component.code === node.component.code
+        ) {
+          this.selectedComponents.delete(selectedNode);
+          this._onDidChangeTreeData.fire();
+          this.notifySelectionChange();
+          return true;
+        }
+      }
+    }
+    // Fallback to direct reference check
+    if (this.selectedComponents.has(node)) {
+      this.selectedComponents.delete(node);
+      this._onDidChangeTreeData.fire();
+      this.notifySelectionChange();
+      return true;
+    }
+    return false;
+  }
+
+  clearAll() {
+    this.selectedComponents.clear();
+    this._onDidChangeTreeData.fire();
+    this.notifySelectionChange();
+  }
+
+  onSelectionChange(listener: () => void) {
+    this.selectionChangeListeners.push(listener);
+  }
+
+  private notifySelectionChange() {
+    this.selectionChangeListeners.forEach((listener) => listener());
   }
 }
 
@@ -67,9 +151,15 @@ export async function refreshComponentView(
     const pkg = item.package;
     const mod = item.module;
 
-    if (!grouped[type]) grouped[type] = {};
-    if (!grouped[type][pkg]) grouped[type][pkg] = {};
-    if (!grouped[type][pkg][mod]) grouped[type][pkg][mod] = [];
+    if (!grouped[type]) {
+      grouped[type] = {};
+    }
+    if (!grouped[type][pkg]) {
+      grouped[type][pkg] = {};
+    }
+    if (!grouped[type][pkg][mod]) {
+      grouped[type][pkg][mod] = [];
+    }
 
     grouped[type][pkg][mod].push(item);
   }
@@ -78,26 +168,41 @@ export async function refreshComponentView(
     ([type, packages]) =>
       new TreeNode(
         type,
+        type,
+        "",
         "rootNode",
         vscode.TreeItemCollapsibleState.Collapsed,
         Object.entries(packages).map(
           ([pkg, modules]) =>
             new TreeNode(
               pkg,
+              type,
+              "",
               "folderNode",
               vscode.TreeItemCollapsibleState.Collapsed,
               Object.entries(modules).map(
                 ([mod, items]) =>
                   new TreeNode(
                     mod,
+                    type,
+                    "",
                     "folderNode",
                     vscode.TreeItemCollapsibleState.Collapsed,
                     items.map(
                       (item) =>
                         new TreeNode(
                           `${item.package}${item.module}${item.code}`,
+                          type,
+                          "",
                           "leafNode",
-                          vscode.TreeItemCollapsibleState.None
+                          vscode.TreeItemCollapsibleState.None,
+                          [],
+                          {
+                            type: item.type,
+                            package: item.package,
+                            module: item.module,
+                            code: item.code,
+                          }
                         )
                     )
                   )
@@ -108,4 +213,65 @@ export async function refreshComponentView(
   );
 
   dataProvider.refresh(tree);
+}
+
+export class SelectedComponentsDataProvider
+  implements vscode.TreeDataProvider<TreeNode>
+{
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    TreeNode | undefined | null | void
+  > = new vscode.EventEmitter<TreeNode | undefined | null | void>();
+  readonly onDidChangeTreeData: vscode.Event<
+    TreeNode | undefined | null | void
+  > = this._onDidChangeTreeData.event;
+
+  constructor(private componentProvider: ComponentDataProvider) {
+    // Listen to selection changes in the main component provider
+    componentProvider.onSelectionChange(() => {
+      this._onDidChangeTreeData.fire();
+    });
+  }
+
+  refresh() {
+    this._onDidChangeTreeData.fire();
+  }
+
+  getTreeItem(element: TreeNode): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: TreeNode): Thenable<TreeNode[]> {
+    if (!element) {
+      // Root level: group by component type
+      const selectedNodes = this.componentProvider.getSelectedNodes();
+      const groupedByType: Record<string, TreeNode[]> = {};
+
+      selectedNodes.forEach((node) => {
+        if (node.component) {
+          const type = node.component.type;
+          if (!groupedByType[type]) {
+            groupedByType[type] = [];
+          }
+          groupedByType[type].push(node);
+        }
+      });
+
+      const tree = Object.entries(groupedByType).map(([type, nodes]) => {
+        return new TreeNode(
+          type,
+          type,
+          `(${nodes.length})`,
+          "rootNode",
+          vscode.TreeItemCollapsibleState.Collapsed,
+          nodes,
+          undefined
+        );
+      });
+
+      return Promise.resolve(tree);
+    } else {
+      // Return children for folder nodes (types)
+      return Promise.resolve(element.children);
+    }
+  }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,130 @@
 import * as vscode from "vscode";
-import { ComponentDataProvider, refreshComponentView, TreeNode } from "./component-data-provider";
+import * as path from "path";
+import * as fs from "fs";
+import axios from "axios";
+import AdmZip from "adm-zip";
+import { ComponentDataProvider, refreshComponentView, TreeNode, SelectedComponentsDataProvider } from "./component-data-provider";
 
 export function activate(context: vscode.ExtensionContext) {
   const componentExplorerProvider = new ComponentDataProvider();
+  const selectedComponentsProvider = new SelectedComponentsDataProvider(componentExplorerProvider);
 
   vscode.window.registerTreeDataProvider("component-explorer", componentExplorerProvider);
+  vscode.window.registerTreeDataProvider("selected-components", selectedComponentsProvider);
+  
   vscode.commands.registerCommand("component-explorer.refresh", async () => await refreshComponentView(componentExplorerProvider));
-  vscode.commands.registerCommand("component-explorer.import", (node: TreeNode) => vscode.window.showInformationMessage(`Successfully called Import entry on ${node.label}.`));
+  
+  vscode.commands.registerCommand("component-explorer.select", async (node?: TreeNode) => {
+    if (node && node.contextType === "leafNode") {
+      componentExplorerProvider.toggleSelection(node);
+      const isSelected = componentExplorerProvider.isSelected(node);
+      vscode.window.showInformationMessage(
+        `${isSelected ? "Selected" : "Deselected"} ${node.label}`
+      );
+      // selectedComponentsProvider will automatically refresh via listener
+    } else {
+      // If called on a folder/root or without node, import all selected components
+      await importComponents(componentExplorerProvider);
+    }
+  });
+
+  // Import command for selected components view
+  vscode.commands.registerCommand("selected-components.import", async () => {
+    await importComponents(componentExplorerProvider);
+  });
+
+  // Remove command for selected components view
+  vscode.commands.registerCommand("selected-components.remove", async (node?: TreeNode) => {
+    if (node && node.component) {
+      const removed = componentExplorerProvider.removeSelectedNode(node);
+      if (removed) {
+        vscode.window.showInformationMessage(`Removed ${node.label} from selection`);
+        // selectedComponentsProvider will automatically refresh via listener
+      }
+    }
+  });
+
+  // Add a command to trigger import of all selected components
+  vscode.commands.registerCommand("component-explorer.importSelected", async () => {
+    await importComponents(componentExplorerProvider);
+  });
+}
+
+async function importComponents(selectedProvider: ComponentDataProvider) {
+  const components = selectedProvider.getSelectedComponents();
+
+  if (components.length === 0) {
+    vscode.window.showWarningMessage("No components selected for import.");
+    return;
+  }
+
+  const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+  if (!workspaceFolder) {
+    vscode.window.showErrorMessage("No workspace folder found.");
+    return;
+  }
+
+  const developmentFolder = path.join(workspaceFolder.uri.fsPath, "Development");
+  if (!fs.existsSync(developmentFolder)) {
+    fs.mkdirSync(developmentFolder, { recursive: true });
+  }
+
+  const progressOptions: vscode.ProgressOptions = {
+    location: vscode.ProgressLocation.Notification,
+    title: "Importing Components",
+    cancellable: false,
+  };
+
+  await vscode.window.withProgress(progressOptions, async (progress) => {
+    try {
+      progress.report({
+        increment: 0,
+        message: `Sending ${components.length} component(s) to server...`,
+      });
+
+      // Send POST request to /import endpoint
+      const response = await axios.post(
+        "http://localhost:3000/import",
+        { components },
+        {
+          responseType: "arraybuffer",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      progress.report({
+        increment: 50,
+        message: "Received zip file, extracting...",
+      });
+
+      // Extract zip file
+      const zip = new AdmZip(Buffer.from(response.data));
+      zip.extractAllTo(developmentFolder, true);
+
+      progress.report({
+        increment: 100,
+        message: "Extraction complete!",
+      });
+
+      // Clear selected components after successful import
+      selectedProvider.clearAll();
+
+      vscode.window.showInformationMessage(
+        `Successfully imported ${components.length} component(s) to Development folder`
+      );
+    } catch (error: any) {
+      console.error("Error importing components:", error);
+      const errorMessage =
+        error.response?.data
+          ? Buffer.from(error.response.data).toString("utf-8")
+          : error.message;
+      vscode.window.showErrorMessage(
+        `Failed to import components: ${errorMessage}`
+      );
+    }
+  });
 }
 
 export function deactivate() {}
